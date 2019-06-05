11b6265e347722df117cba5c42e7ff1f80d0d852
truong.nt <truong.nt@outlook.com.vn>
2019-06-05 17:34:10 +0700

    update game logic

--- controller/GameOne.js
+++ controller/GameOne.js
@@ -7,10 +7,16 @@ class GameOne{
         var updateTask = null;
         var LIST_PLAYER = {};
         var keys = Object.keys(room.LIST_MEMB);
+        var quest; 
+        var questNumber =0;
         keys.forEach(element=>{
             var memb = room.LIST_MEMB[element];
             var player = Player({socket:memb.socket,health:5});
             LIST_PLAYER[player.id]= player;
+            player.socket.on("sendAnswer",(data)=>{
+                player.questid = data.id;
+                player.answer = data.answer;
+            });
         });
 
         this.startGame = function(){
@@ -46,13 +52,30 @@ class GameOne{
                 }
                  
             }else if(status === 'start'){
-                Question(count);
+                GameLoop(count);
+            }else if(status === 'end'){
+               // Check Winner
+               keys.forEach(element=>{
+                var pl = LIST_PLAYER[element];
+                pl.gameResult({
+                    win:(pl.health>0),
+                    number:questNumber
+                });
+            });
             }
         };
 
-        function Question(count){
+        function GameLoop(count){
             if(count == 0){
                 // Handle send question
+                quest = Question({
+                    id :(questNumber+1),
+                    type : "text",
+                    quest : "What 's exactly?",
+                    answer : ["A Answer","B Answer","C Answer","D Answer"],
+                    correct : 1,  // "B Answer"
+                });
+
             }else if(count <50){
                 // Handle send time
                 if(count%5 ==0){
@@ -62,13 +85,25 @@ class GameOne{
                 }
             }else{
                 // Kiem tra dap ap cua nguoi choi
+                var endGame = false;
+                keys.forEach(element=>{
+                    var pl = LIST_PLAYER[element];
+                    var plHealth = pl.checkAnswer(quest);
+                    if(plHealth == 0)
+                        endGame = true;
+                });
+
+                if(endGame)
+                    status = "end";
+                else{
+                    if(count >= 60){
+                        count = -1;
+                    }
+                }
             }
 
             count++;
         };
-
-        // Handle user send Answer
-        
 
     }
     
@@ -79,16 +114,43 @@ Player = function(data){
         id: data.socket.id,
         socket: data.socket,
         health : data.health,
-        answer : "",
-        subHealth = function(){
-            health -=1;
+        questid: "",
+        answer : -1,
+
+        checkAnswer = function(quest){
+            if(questid != quest.id || answer != quest.correct){
+                health -=1;
+            }
+            socket.emit("checkAnswer",{
+                correct : correct,
+                health:health,
+            });
             if(health<=0){
                 return false;
             }else{
                 return true;
             }
+        },
+
+        gameResult = function(data){
+            socket.emit("gameResult",{
+                win: data.win,
+                number: data.number,
+            });
         }
+        
     },
+    return self;
+}
+
+Question = function(data){
+    var self = {
+        id = data.id,
+        type = data.type,
+        quest = data.quest,
+        answers = data.answers,
+        correct = data.correct
+    }
     return self;
 }
 
