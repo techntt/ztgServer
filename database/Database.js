ec8c52ddee4573ecc116702d838847cd300c8a6c
truong.nt <truong.nt@outlook.com.vn>
2019-06-06 17:33:49 +0700

    add database methods

--- database/Database.js
+++ database/Database.js
@@ -2,12 +2,51 @@ var mongo = require('mongojs');
 class Database {
     constructor(){
     var uri = "mongodb://admin:admin@cluster0-shard-00-00-tzr0l.mongodb.net:27017,cluster0-shard-00-01-tzr0l.mongodb.net:27017,cluster0-shard-00-02-tzr0l.mongodb.net:27017/camvan?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-    var database = mongo(uri);
+    var db = mongo(uri);
     console.log("Database Connected: "+database);
     
+    this.isValidUser = function(data,callback){
+        db.user.find({username : data.username, password : data.password},function(err,res){
+            if(res.length>0)
+                callback(true);
+            else
+                callback(false);
+            });
+    };
+
+    this.hasUserName = function(data,callback){
+        db.user.find({username: data.username},function(err,res){
+            if(res.length>0)
+                callback(true);
+            else
+                callback(false);
+        });
+    };
+
+    this.addUser = function(data,callback){
+        db.user.insert({username:data.username, password:data.password,email: data.email,status: 1},function(err){
+            console.log("DB create user: "+data.username);
+            callback();
+        });
+    };
+
+    this.changeStatusUser = function(data,callback){
+        db.user.update({username:data.username},{$set:{status:data.status}},function(err){
+            console.log("Change "+data.username+" status : "+data.status);
+            callback();
+        });
+    };
+
+    this.deleteUser = function(data,callback){
+        db.user.remove({username:data.username,password:data.password},function(err){
+            callback();
+        })
+    },
+
     this.CloseDB = function(){
         database.close();
-    }
+    };
+
     };
 
 }
